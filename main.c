f1c5cab5c8461ce24750aaf27183468ef38fe08b
Felix DEAGE <fdeage@gmail.com>
2015-02-12 15:21:18 +0100

    automatic commit: Thu Feb 12 15:21:18 CET 2015

--- main.c
+++ main.c
@@ -6,15 +6,24 @@
 /*   By: fdeage <fdeage@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/26 21:20:58 by fdeage            #+#    #+#             */
-/*   Updated: 2015/02/05 13:12:02 by fdeage           ###   ########.fr       */
+/*   Updated: 2015/02/12 15:20:34 by fdeage           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 #include "get_next_line.h"
 #include "libft.h"
+
+
+static void find_leaks(void)
+{
+	system("leaks `ps | grep ./$1 | head -n 1 | cut -d' ' -f1`");
+	return ;
+}
 
 static void ft_put_string_nbr(int nbr, char *dest)
 {
@@ -41,7 +50,7 @@ static void ft_put_string_nbr(int nbr, char *dest)
 
 char        *ft_itoatab(int nbr, char *tab)
 {
-    ft_bzero(tab, 12);
+    bzero(tab, 12);
     if (nbr == -2147483648)
     {
         ft_put_string_nbr(nbr / 10, tab);
@@ -54,17 +63,17 @@ char        *ft_itoatab(int nbr, char *tab)
 
 int     main(int ac, char **av)
 {
-	char		*line;
 	int			fds[40];
 	int			ret[41];
 	int			i;
 	int			all_empty;
 	char		color_string[100] = {0};
 	char		tab[15] = {0};
+	char		*line;
 
 	if (ac < 2)
 	{
-		printf("Please type at least 1 filename as params: %s file1 [file2] ... [block]\n", av[0]);
+		printf("Please type at least 1 filename as params: %s file1 [file2] ... [leaks]\n", av[0]);
 		return (EXIT_FAILURE);
 	}
 	else if (ac >= 40)
@@ -74,17 +83,22 @@ int     main(int ac, char **av)
 	}
 	else
 	{
+
+		/*
+		** reads all the files sequentially
+		*/
+
 		i = 1;
 		while (i < ac)
 		{
-			if (ft_strcmp(av[i], "stdin") == 0)
+			if (strcmp(av[i], "stdin") == 0)
 			{
 				ret[i] = 1;
 				fds[i] = 0;
 				++i;
 				continue ;
 			}
-			else if (i == ac - 1 && ft_strcmp(av[i], "block") == 0)
+			else if (i == ac - 1 && strcmp(av[i], "leaks") == 0)
 			{
 				ret[i] = 2;
 				++i;
@@ -112,13 +126,13 @@ int     main(int ac, char **av)
 					line[ret[40] + 3] = '\0';
 				}
 
-				ft_strcpy(color_string, "MAIN:  test file= %s\n.\033[");
-				ft_strcat(color_string, ft_itoatab(31 + i % 7, tab));
-				ft_strcat(color_string, "m%s\033[0m.\nnb_car(file) = %d\n\n");
+				strcpy(color_string, "MAIN:  test file= %s\n.\033[");
+				strcat(color_string, ft_itoatab(31 + i % 7, tab));
+				strcat(color_string, "m%s\033[0m.\nnb_car(file) = %d\n\n");
 
-				printf(color_string, av[i], line, (int)ft_strlen(line));
+				printf(color_string, av[i], line, (int)strlen(line));
 				lseek(fds[i], 0, SEEK_SET);
-				(void)ft_memset(line, '\0', ft_strlen(line));
+				(void)memset(line, '\0', strlen(line));
 				free(line);
 				ret[i] = 1;
 				++i;
@@ -126,6 +140,9 @@ int     main(int ac, char **av)
 		}
 	}
 
+	/*
+	** reads one line of each file sequentially
+	*/
 
 	while (1)
 	{
@@ -133,18 +150,18 @@ int     main(int ac, char **av)
 		all_empty = 1;
 		while (i < ac)
 		{
-			//printf("ret[%d] = %d, fd[%d] = %d\n", i, ret[i], i, fds[i]);
 			if (ret[i] == 1)
 			{
 				all_empty = 0;
 				ret[i] = get_next_line(fds[i], &line);
 
-				ft_strcpy(color_string, "MAIN%d:  ret=\033[33m%d\033[0m, line=.\033[");
-                ft_strcat(color_string, ft_itoatab(31 + i % 7, tab));
-				ft_strcat(color_string, "m%s\033[0m. len=%d\n");
+				strcpy(color_string, "MAIN%d:  ret=\033[33m%d\033[0m, line=.\033[");
+                strcat(color_string, ft_itoatab(31 + i % 7, tab));
+				strcat(color_string, "m%s\033[0m. len=%d\n");
 
-				printf(color_string, i, ret[i], line, (int)ft_strlen(line));
-				free(line);
+				printf(color_string, i, ret[i], line, (int)strlen(line));
+				if (ret[i] > 0)
+					free(line);
 				line = NULL;
 			}
 			++i;
@@ -153,7 +170,13 @@ int     main(int ac, char **av)
 			break ;
 	}
 
-	if (!ft_strcmp(av[ac - 1], "block"))
-		while(1);
+	/*
+	** if "leaks" is typed, check the memory leaks with /usr/bin/leaks
+	** using the "system()" function
+	*/
+
+	if (!strcmp(av[ac - 1], "leaks"))
+		find_leaks();
+
 	return (EXIT_SUCCESS);
 }
